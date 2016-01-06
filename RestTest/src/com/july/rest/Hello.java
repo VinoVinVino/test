4c542dbaa9478592ffc492e45bf323a6465d63d6
VinoVinVino <vinsh07@gmail.com>
2016-01-06 17:41:37 +0530

    Initial Import

---
+++ RestTest/src/com/july/rest/Hello.java
@@ -0,0 +1,36 @@
+package com.july.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Welcome";
+
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Welcome" + "</hello>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Welcome" + "</title>" + "<body><h1>" + "Hello Welcome" + "</body></h1>"
+				+ "</html> ";
+	}
+	
+	@Path("/welcome")
+	public void writeWelcome(){
+		
+	}
+
+}
