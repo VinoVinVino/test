049a49e5eaf96fd9b4d9619a1bd338498bbc54ac
VinoVinVino <vinsh07@gmail.com>
2016-01-07 14:29:34 +0530

    Welcome Section

--- RestTest/src/com/july/rest/Hello.java
+++ RestTest/src/com/july/rest/Hello.java
@@ -11,26 +11,25 @@ public class Hello {
 	@GET 
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayPlainTextHello() {
-		return "Hello Welcome";
-
+		return "Hello Welcome to Rest Application";
 	}
 
 	@GET
 	@Produces(MediaType.TEXT_XML)
 	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Welcome" + "</hello>";
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Welcome to Rest Application" + "</hello>";
 	}
 
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String sayHtmlHello() {
-		return "<html> " + "<title>" + "Hello Welcome" + "</title>" + "<body><h1>" + "Hello Welcome" + "</body></h1>"
+		return "<html> " + "<title>" + "Hello Welcome to Rest Application" + "</title>" + "<body><h1>" + "Hello Welcome to Rest Application" + "</body></h1>"
 				+ "</html> ";
 	}
 	
 	@Path("/welcome")
-	public void writeWelcome(){
-		
+	public String writeWelcome(){
+		return "Hello Welcome to Rest Application in Welcome Page";
 	}
 
 }
